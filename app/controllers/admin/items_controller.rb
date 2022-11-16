c8e46ce84f9db1235d5aad0306c186035ea80855
ayya-kh <76466532+ayya-kh@users.noreply.github.com>
2022-11-16 10:57:21 +0000

    2022/11/16

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,35 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def updated
+    @item = Item.find(params[:id])
+  end
+
+  private
+	def item_params
+	  params.require(:item).permit(:name, :introduction, :image, :is_active, :price, :genre_id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,10 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    get "top" => "homes#top"
+  end
+
 end
