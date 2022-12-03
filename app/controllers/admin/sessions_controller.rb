c977dbc8f375987d5441ae667a6dad75a6e8d142
ayya-kh <76466532+ayya-kh@users.noreply.github.com>
2022-12-03 14:49:49 +0000

    2022/12/03

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -4,7 +4,7 @@ class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
-      admin_top_path(resource)
+      admin_root_path(resource)
   end
 
   # GET /resource/sign_in
@@ -21,6 +21,10 @@ class Admin::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 
   # protected
 
